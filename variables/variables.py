d74a74f00bcdfb590ea51c252ef6909287fbb0fb
Christian <soouzachristian@gmail.com>
2026-02-12 20:02:51 -0300

    refactor(variables): Ajustando o arquivo de variaveis

--- variables/variables.py
+++ variables/variables.py
@@ -1,52 +1,63 @@
-
-
 import os
-
+from utils.read_json_file import read_json_file
 
 PATH_TEMPLATES = os.path.join(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "templates"
 )
 
+DATA_OUTPUT = read_json_file(
+    os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'output.json')
+)
+
+PROJECT_NAME = "metabase"
+ENVIRONMENT = 'hml'
 
 ecs_variables = {
     "stack_name": "ecs-stack-preview",
     "template_path": os.path.join(PATH_TEMPLATES, "ecs-stack.yaml"),
     "parameters": {
-        "VpcId": "vpc-0ca2dd29eb23bc02d",
-        "SubnetIds": "subnet-07b5e4eea26504a2d,subnet-08437a2c2e38d53da",
-        "SecurityGroupIds": "sg-034e3df3d4a62f807",
-        "ContainerImage": "484907521567.dkr.ecr.us-east-1.amazonaws.com/my-app-repo:latest",
+        "ProjectName": PROJECT_NAME,
+        "Environment": ENVIRONMENT,
+        "VpcId": DATA_OUTPUT["VpcId"],
+        "SubnetIds": ",".join(DATA_OUTPUT["SubnetIds"]),
+        "SecurityGroupIds": ",".join(DATA_OUTPUT["SecurityGroupIds"]),
+        "ContainerImage": DATA_OUTPUT["ContainerImage"],
         "ContainerCpu": 1024,
         "ContainerMemory": 2048,
         "TaskFamily": "metabase-task",
         "ContainerName": "metabase",
         "ContainerPort": 3000,
-        "Environment": "prod",
         "NetworkMode": "awsvpc",
         "OperatingSystemFamily": "LINUX",
-        "RdsEndpoint": "metabase-db.copgisqwgh9a.us-east-1.rds.amazonaws.com",
+        "RdsEndpoint": DATA_OUTPUT["RdsEndpoint"],
         "RdsDbName": "metabase",
-        "DatabaseSecretArn": "arn:aws:secretsmanager:us-east-1:484907521567:secret:metabase/db-bAct6K",
+        "DatabaseSecretArn": DATA_OUTPUT["DatabaseSecretArn"],
         "MaxTaskCount": 4,
-        "TargetGroupArn": "arn:aws:elasticloadbalancing:us-east-1:484907521567:targetgroup/metabasegroup/3edcb409d9f80e17"
+        "TargetGroupArn": DATA_OUTPUT["TargetGroupArn"]
     }
 }
 
 vpc_variables = {
     "stack_name": "vpc-stack-preview",
     "template_path": os.path.join(PATH_TEMPLATES, "vpc-stack.yaml"),
+    "parameters": {
+        "ProjectName": PROJECT_NAME,
+        "Environment": ENVIRONMENT,
+    }
 }
 
 rds_variables = {
     "stack_name": "rds-stack-preview",
     "template_path": os.path.join(PATH_TEMPLATES, "rds-stack.yaml"),
     "parameters": {
+        "ProjectName": PROJECT_NAME,
+        "Environment": ENVIRONMENT,
         "DBUser": os.getenv("DATABASE_USER"),
         "DBPassword": os.getenv("DATABASE_PASSWORD"),
         "DBName": os.getenv("DATABASE_NAME"),
-        "VpcId": "vpc-0ca2dd29eb23bc02d",
-        "PrivateSubnets": "subnet-07b5e4eea26504a2d,subnet-08437a2c2e38d53da",
-        "ECSSecurityGroup": "sg-034e3df3d4a62f807"
+        "VpcId": DATA_OUTPUT["VpcId"],
+        "PrivateSubnets": ",".join(DATA_OUTPUT["PrivateSubnets"]),
+        "ECSSecurityGroup": DATA_OUTPUT["ECSSecurityGroup"]
     }
 }
 
@@ -54,7 +65,9 @@ sgs_alb_variables = {
     "stack_name": "sgs-alb-stack-preview",
     "template_path": os.path.join(PATH_TEMPLATES, "security-groups/sgs-alb-stack.yaml"),
     "parameters": {
-        "VpcId": "vpc-0ca2dd29eb23bc02d",
+        "ProjectName": PROJECT_NAME,
+        "Environment": ENVIRONMENT,
+        "VpcId": DATA_OUTPUT["VpcId"],
     }
 }
 
@@ -62,8 +75,10 @@ sgs_ecs_variables = {
     "stack_name": "sgs-ecs-stack-preview",
     "template_path": os.path.join(PATH_TEMPLATES, "security-groups/sgs-ecs-stack.yaml"),
     "parameters": {
-        "VpcId": "vpc-0ca2dd29eb23bc02d",
-        "ALBSecurityGroup": "sg-0a84b9c7621334f6e",
+        "ProjectName": PROJECT_NAME,
+        "Environment": ENVIRONMENT,
+        "VpcId": DATA_OUTPUT["VpcId"],
+        "ALBSecurityGroup": DATA_OUTPUT["ALBSecurityGroup"],
     }
 }
 
@@ -71,8 +86,10 @@ sgs_rds_variables = {
     "stack_name": "sgs-rds-stack-preview",
     "template_path": os.path.join(PATH_TEMPLATES, "security-groups/sgs-rds-stack.yaml"),
     "parameters": {
-        "VpcId": "vpc-0ca2dd29eb23bc02d",
-        "ECSSecurityGroup": "sg-034e3df3d4a62f807"
+        "ProjectName": PROJECT_NAME,
+        "Environment": ENVIRONMENT,
+        "VpcId": DATA_OUTPUT["VpcId"],
+        "ECSSecurityGroup": DATA_OUTPUT["ECSSecurityGroup"],
     }
 }
 
@@ -80,6 +97,8 @@ ecr_variables = {
     "stack_name": "ecr-repository",
     "template_path": os.path.join(PATH_TEMPLATES, "ecr-stack.yaml"),
     "parameters": {
+        "ProjectName": PROJECT_NAME,
+        "Environment": ENVIRONMENT,
         "RepositoryName": "my-app-repo",
     }
 }
@@ -88,8 +107,10 @@ vpc_endpoint_variables = {
     "stack_name": "vpc-endpoint-stack-preview",
     "template_path": os.path.join(PATH_TEMPLATES, "vpc-endpoint-stack.yaml"),
     "parameters": {
-        "VpcId": "vpc-0ca2dd29eb23bc02d",
-        "SubnetIds": ["subnet-07b5e4eea26504a2d", "subnet-08437a2c2e38d53da"],
-        "SecurityGroupIds": ["sg-034e3df3d4a62f807"]
+        "ProjectName": PROJECT_NAME,
+        "Environment": ENVIRONMENT,
+        "VpcId": DATA_OUTPUT["VpcId"],
+        "SubnetIds": ",".join(DATA_OUTPUT["PrivateSubnets"]),
+        "SecurityGroupIds": ",".join(DATA_OUTPUT["ECSSecurityGroup"])
     }
 }
